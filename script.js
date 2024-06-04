1a3b203ff3338dfac74e038cd2e2f5a76197ff6f
“darek632” <162553961+darek632@users.noreply.github.com>
2024-06-04 18:37:27 +0100

    add logic for playing single round and score incrementation.

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ console.log('Open the console to play rock, paper, scissors') ;
     let result = Math.floor(Math.random() * (max-min +1) + min); // gets random number between default parameters 1 and 3
   
 if (result == 1) {
-    console.log(result);
+    console.log(result, outcome);
     outcome = "rock";
     return outcome ; } // if result = 1 print 'rock'
 
 else if (result==2) 
     {
-        console.log(result);
+        console.log(result, outcome);
     outcome = "paper";
  return outcome ;} // if result = 2 print 'paper'
 
 else {
     outcome = "scissors" 
-    console.log(result);;
+    console.log(result, outcome);;
      return outcome; } ; // else (meaning if result = 3) print 'scissors'
 }
 
@@ -63,7 +63,59 @@ else {alert('not a valid choice, please type rock, paper or scissors') ; }
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound (humanChoice,computerChoice) { 
 
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        ++humanScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You win! Rock beats scissors`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        ++computerScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You lose! Paper beats rock.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        ++humanScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You win! Paper beats rock.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        ++computerScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You lose! Scissors beats paper.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        ++humanScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You win! Scissors beats paper.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        ++computerScore;
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`You lose! Scissors beats rock.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+    else if (humanChoice == computerChoice) {
+        
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
+        console.log(`It\'s a draw. Play again.`);
+        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
+    }
+
+
+
+
+
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
 
 
 // ask user for choice 
