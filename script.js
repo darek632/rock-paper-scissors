7c496b592afc0c0a111bfea453241c9e0e00058a
“darek632” <162553961+darek632@users.noreply.github.com>
2024-06-06 11:06:45 +0100

    add function to play 5 rounds and declare winner at end.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log('Open the console to play rock, paper, scissors') ;
 
+alert(`The game will be played in the console. Open the console, type: 'playGame()'. A new pop up will appear, initiating the first round of the game.`) //creates a pop telling the user to open the console to start the game. 
 // write a code that will randomly return rock paper or scissors 
 
 
@@ -22,24 +22,24 @@ console.log('Open the console to play rock, paper, scissors') ;
 
 
  function getComputerChoice() {
-    min=1,max=3;
-    let outcome; 
+    let min=1 ; let max=3; // declare minimum and maximum values for random number generator formula.
+    let outcome; // local variable for storing the random generation. 
     let result = Math.floor(Math.random() * (max-min +1) + min); // gets random number between default parameters 1 and 3
   
 if (result == 1) {
-    console.log(result, outcome);
-    outcome = "rock";
+    // console.log(result, outcome);
+    outcome = "rock"; 
     return outcome ; } // if result = 1 print 'rock'
 
 else if (result==2) 
     {
-        console.log(result, outcome);
+        // console.log(result, outcome);
     outcome = "paper";
  return outcome ;} // if result = 2 print 'paper'
 
 else {
     outcome = "scissors" 
-    console.log(result, outcome);;
+    // console.log(result, outcome);;
      return outcome; } ; // else (meaning if result = 3) print 'scissors'
 }
 
@@ -47,17 +47,17 @@ else {
 function getHumanChoice() {
 let human = prompt("What\'s your weapon of choice?", "rock, paper or scissors");
 let answer; 
-if (human.toLowerCase() === "rock") 
+if (human.toLowerCase() == "rock") 
     {answer = 'rock' ; 
         return answer;
     }
-else if (human.toLowerCase() === "paper") {
+else if (human.toLowerCase() == "paper") {
     answer = "paper"; 
     return answer; } 
-else if (human.toLowerCase() === "scissors") {
+else if (human.toLowerCase() ==  "scissors") {
     answer = "scissors" ;
     return answer ; }
-else {alert('not a valid choice, please type rock, paper or scissors') ; }
+else  {alert('not a valid choice, please type rock, paper or scissors') ; }
 }
 
 let humanScore = 0;
@@ -104,18 +104,66 @@ function playRound (humanChoice,computerChoice) {
     else if (humanChoice == computerChoice) {
         
         console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`) ;
-        console.log(`It\'s a draw. Play again.`);
+        console.log(`It\'s a draw.`);
         console.log(`Your score: ${humanScore}. Computer score: ${computerScore}.`) ;
     }
 
-
-
-
-
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+
+
+
+// -  The function needs to reset the humanChoice and computerChoice of first round. 
+function playGame() { 
+    for (var i = 1; i < 6; i++) { // for (var i = 0; i < 5; i++)
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+    playRound(humanChoice,computerChoice);
+    console.log(`Round ${i} complete.`)
+    }
+   if (humanScore > computerScore) { 
+    console.log(`You win :) Human: ${humanScore} vs Computer: ${computerScore}.`) ;
+   }
+   else if ( humanScore < computerScore) { 
+    console.log(`You lose :( Human: ${humanScore} vs. Computer: ${computerScore}`) ;
+   }
+   else if (humanScore == computerScore) {
+    console.log(`It\'s a draw :| Human: ${humanScore} vs. Computer: ${computerScore}. No winners today. call the playGame() function to start a new game`) ;
+   }
+    
+  
+}
+
+
+
+// why is it asking me for my weapon of choice twice for each round, but only noting down the second time. 
+
+   
+    // function endResult () {
+    //     if (noOfRounds == 5) {
+    //          console.log(`Human: ${humanScore} vs. Computer: ${computerScore}. Game over buddy, the scores don\'t lie.`) ;
+    //                         }
+       
+        
+    
+
+    // else if (i==5 && computerScore > humanScore) {
+    //     console.log`Game over. You lose! Human:${humanScore} vs Computer:${computerScore}`
+    // }
+    // else if(i==5 && computerScore == humanScore) {
+    //     console.log(`Game over. It\'s a draw. Human: ${humanScore} vs computer: ${computerScore}`);
+    // }
+
+    
+    
+
+
+// - It then needs to generate a new computerChoice and ask the human again for their choice. 
+// - These new choices then need to be passed into playRound function to play a game. 
+// - The no. of rounds starts at 0. Each time playRound is played, score goes up by 1. 
+// - The steps above will repeat until no. of rounds = 5.
+// - When no. of rounds = 5, print result message, including human score and computer score. 
+
 
 
 // ask user for choice 
